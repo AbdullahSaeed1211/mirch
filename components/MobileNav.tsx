6858fdd1fc412dc971916bedca5df59678553156
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2024-06-05 16:00:08 +0530

    nav links

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -16,7 +16,7 @@ return  <Sheet>
   </Button>
 </SheetTrigger>
 <SheetContent side="left" className="bg-white">
-  <Link className="flex items-center gap-2 text-black" href="#">
+  <Link className="flex items-center gap-2 text-black" href="/">
     <Image src={mirchLogo} alt='mirch logo' />
   </Link>
   <div className="grid gap-4 py-6">
@@ -33,13 +33,13 @@ return  <Sheet>
       </CollapsibleTrigger>
       <CollapsibleContent>
         <div className="-mx-6 grid gap-6 p-6">
+          <Link className="group grid h-auto w-full justify-start gap-1" href="/services">
+            <div className="text-sm font-medium leading-none group-hover:underline text-black">Services Page</div>
+            <div className="line-clamp-2 text-sm leading-snug text-gray-500">Explore our other service offerings</div>
+          </Link>
           <Link className="group grid h-auto w-full justify-start gap-1" href="/content-marketing">
             <div className="text-sm font-medium leading-none group-hover:underline text-black">Content Marketing</div>
-            <div className="line-clamp-2 text-sm leading-snug text-gray-500">Elevate your brand with our content marketing services.</div>
-          </Link>
-          <Link className="group grid h-auto w-full justify-start gap-1" href="#">
-            <div className="text-sm font-medium leading-none group-hover:underline text-black">Item 2</div>
-            <div className="line-clamp-2 text-sm leading-snug text-gray-500">Explore our other service offerings.</div>
+            <div className="line-clamp-2 text-sm leading-snug text-gray-500">Elevate your brand with our content marketing services</div>
           </Link>
           <Link className="group grid h-auto w-full justify-start gap-1" href="#">
             <div className="text-sm font-medium leading-none group-hover:underline text-black">Item 3</div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <header className="flex h-20 w-full items-center px-4 md:px-6 bg-[#000b47]">
       <div className="flex-1">
-        <Link className="flex items-center gap-2 text-white" href="#">
+        <Link className="flex items-center gap-2 text-white" href="/">
           <Image src={mirchLogo} alt="mirch logo" />
         </Link>
       </div>
