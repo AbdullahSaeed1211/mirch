614fc8ddd5ae5177a3a9334dd36db4e22d256169
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2024-06-05 16:01:45 +0530

    Page: Marketing Strategy init

---
+++ app/marketing-strategy/page.tsx
@@ -0,0 +1,4 @@
+'use client';
+export default function page() {
+return <div>Hello</div>
+}
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -41,8 +41,8 @@ return  <Sheet>
             <div className="text-sm font-medium leading-none group-hover:underline text-black">Content Marketing</div>
             <div className="line-clamp-2 text-sm leading-snug text-gray-500">Elevate your brand with our content marketing services</div>
           </Link>
-          <Link className="group grid h-auto w-full justify-start gap-1" href="#">
-            <div className="text-sm font-medium leading-none group-hover:underline text-black">Item 3</div>
+          <Link className="group grid h-auto w-full justify-start gap-1" href="/marketing-strategies">
+            <div className="text-sm font-medium leading-none group-hover:underline text-black">Marketing Strategy</div>
             <div className="line-clamp-2 text-sm leading-snug text-gray-500">Discover more about our services.</div>
           </Link>
         </div>
